a80d4b520fc80f352327cf7e55557544c46c032d
headline-design <85418370+headline-design@users.noreply.github.com>
2021-07-04 11:32:17 -0500

    Included build

---
+++ example/build/precache-manifest.73bf247e6aafea2ab0dfd117ad8ac953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc1761ba3a495157a074635d1758c9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dba08a568f2dbcff13b0",
+    "url": "./static/css/main.8af9404e.chunk.css"
+  },
+  {
+    "revision": "d172f73ebf4a890544e0",
+    "url": "./static/js/2.be469c0d.chunk.js"
+  },
+  {
+    "revision": "741da2da7ddac13e2716a2a97e471295",
+    "url": "./static/js/2.be469c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba08a568f2dbcff13b0",
+    "url": "./static/js/main.7312733f.chunk.js"
+  },
+  {
+    "revision": "43da60b2865b47041f95",
+    "url": "./static/js/runtime-main.1ba10e03.js"
+  },
+  {
+    "revision": "c992dc67a2dd62730c9dfd0c23bcb65f",
+    "url": "./static/media/pipeline-express.c992dc67.jpg"
+  }
+]);
